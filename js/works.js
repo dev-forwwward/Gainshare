1d7af860bd4bb1053e26a7c72ab7478b11c47da6
beatriz-araujo <beatriz@forwwward.studio>
2026-05-04 20:05:57 +0100

    Filter button handler update
    Added active state handler

--- js/works.js
+++ js/works.js
@@ -7,4 +7,18 @@ export function works() {
         });
     }
 
+    // filter - active 'all' handler
+    const filterBtns = document.querySelectorAll('.filter_item .tag');
+    const allFilterBtn = document.querySelector('.filter_item.all_filter');
+    if (filterBtns.length > 0) {
+        filterBtns.forEach(btn => {
+            btn.addEventListener('click', () => {
+                let previousActiveBtn = document.querySelector('.filter_item .tag.filter-active');;
+                if (previousActiveBtn) {
+                    previousActiveBtn.classList.remove('filter-active');
+                }
+                btn.classList.add('filter-active');
+            });
+        });
+    }
 }
